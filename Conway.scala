f24eb78f33c909b098d20e1051f305eb438a6729
naniroot <root.nani@gmail.com>
2016-06-28 15:27:08 -0700

    getting random matrix

--- Conway.scala
+++ Conway.scala
@@ -1,4 +1,8 @@
 //Conway Game of Life
+// X => Alive
+// O => dead
+
+import util.Random.nextInt
 
 object Conway{
     def main(args: Array[String]){
@@ -7,15 +11,44 @@ object Conway{
         var mainArray = Array.ofDim[String](numRows, numCols)
         for(i <- 0 to numRows-1){
         	for(j <- 0 to numCols-1){
-        		mainArray(i)(j) = "X"
+        		mainArray(i)(j) = getRandomChar()
         	}
         }
+        println("Initial Matrix")
+        printMatrix(mainArray, numRows, numCols)
+        val iterations = 3
+        getConway(mainArray, numRows, numCols, iterations)
+    }
+    def getConway(mainArray:Array[Array[String]], numRows: Int, numCols: Int, iterations: Int) : Unit = {
+        for(k <- 0 to iterations-1){
+            printf("Printing for iteration %d\n", k+1)
+            for(i<- 0 to numRows-1){
+                for(j<-0 to numCols-1){
 
-        for(i <- 0 to numRows-1){
-        	println("")
-        	for(j <- 0 to numCols-1){
-        		printf(mainArray(i)(j))
-        	}
+                }
+            }
         }
+    }
+    def getNeighbourCount(mainArray:Array[Array[String]], numRows: Int, numCols: Int, row: Int, col: Int) : Int = {
+        var totalNeighborsAlive = 0
+        if()
+    }
+    def printMatrix(mainArray:Array[Array[String]], numRows: Int, numCols: Int) : Unit = {
+        for(i <- 0 to numRows-1){
+            println("")
+            for(j <- 0 to numCols-1){
+                printf(mainArray(i)(j))
+            }
+        }
+        println("")
+    }
+    def getRandomChar() : String = {
+        var randomValue: Int = nextInt
+        val xChar: String = "X"
+        val oChar: String = "O"
+        if(randomValue%2 == 0){
+            return xChar
+        }
+        oChar
     }
 }
